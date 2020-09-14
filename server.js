cdf26d908cf9d5b165187545ecdbfcc5165956d2
prvn-breach <praveenking880@gmail.com>
2020-09-14 15:33:18 +0530

    changes

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use('/api/posts', posts);
 // For production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join('public')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use((req, res, next) => {
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
     });
 }
 
