b9a0828a6a31fe75e2e1126371873bfa69235caf
prvn-breach <praveenking880@gmail.com>
2020-09-14 15:24:45 +0530

    changes

---
+++ public/precache-manifest.bc87a3c4b0c10c3a8458509fdb0b2e06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eff9b65167328f73f227719781855a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "189c50e451a0aef7febd",
+    "url": "/static/css/main.3cb27b3d.chunk.css"
+  },
+  {
+    "revision": "e674aec7dcfe7c38223f",
+    "url": "/static/js/2.9d6e9149.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.9d6e9149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "189c50e451a0aef7febd",
+    "url": "/static/js/main.0ba9701c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5a950c6e1ee9f7385b76d82b3b04bc12",
+    "url": "/static/media/coverpage.5a950c6e.jpeg"
+  }
+]);
